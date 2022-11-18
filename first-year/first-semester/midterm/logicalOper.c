20d8755c82db5bd4e11cd954a3c39d9b2270277b
Jhon Brian Arce <111772143+verastigue18@users.noreply.github.com>
2022-11-18 16:10:12 +0800

    Create logicalOper.c

---
+++ first-year/first-semester/midterm/logicalOper.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 10;
+    int y = 10;
+    int a = 20;
+    int b = 21;
+
+    if(x == y && a != b)
+    {
+        printf("\nx is less than y AND a is equal to b\n");
+    }
+    if(x == y || a == b)
+    {
+        printf("\nx is less than y OR a is equal to b\n");
+    }
+    if(!(x == y && a == b))
+    {
+        printf("\nNOT OPERATION\n");
+    }
+
+    printf("CODES AFTER IF!\n\n");
+    return 0;
+}
